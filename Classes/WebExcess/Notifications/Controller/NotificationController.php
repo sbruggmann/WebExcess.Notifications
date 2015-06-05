dfb08aff6dde5fa61ce29951316742761f0169cf
Stefan Bruggmann <stefan.bruggmann@dotpulse.ch>
2015-06-05 17:52:32 +0200

    fix-authentication

--- Classes/WebExcess/Notifications/Controller/NotificationController.php
+++ Classes/WebExcess/Notifications/Controller/NotificationController.php
@@ -23,31 +23,13 @@ class NotificationController extends \TYPO3\Flow\Mvc\Controller\ActionController
 	public function getAction() {
 		$script = '';
 
-		$notifications = $this->notificationService->getNotifications();
+        $notifications = $this->notificationService->getNotifications();
 		foreach ($notifications as $notification) {
 			$script .= 'window.Typo3Neos.Notification.'.$notification->getType().'(\''.$notification->getTitle().'\', \''.(!empty($notification->getPackageKey())?$notification->getPackageKey().': ':'').$notification->getMessage().'\');';
 		}
 		$this->notificationService->removeNotifications();
 
-		return
-			"console.log('WebExcess.Notifications Check');"
-			.$script;
-	}
-
-	/**
-	 * add a notification
-	 * 
-	 * @param string $type
-	 * @param string $title
-	 * @param string $packageKey
-	 * @param string $message
-	 * 
-	 * @return json
-	 */
-	public function addAction($type, $title, $packageKey='', $message='') {
-		$this->notificationService->addNotification($type, $title, $packageKey, $message);
-
-		return json_encode(array('status' => true));
+		return "console.log('WebExcess.Notifications Check');".$script;
 	}
 
 }
